8e2b8bd02e24127eed8b54430dce5339925138a0
majulong <45955327+majulong@users.noreply.github.com>
2019-07-31 03:08:45 -0700

    Update nedis.lua

--- nedis/nedis.lua
+++ nedis/nedis.lua
@@ -152,7 +152,7 @@ local function get_all_curr_master()
         end
 
 	-- 获取当前sentinel_list内的所有master_name
-	local res, err = red:sentinel("slaves",mymaster)
+	local res, err = red:sentinel("master", mymaster)
 	if err then
 		ngx.log(ngx.ERR,"redis execution [sentinel masters] error :",err)
 		return false
